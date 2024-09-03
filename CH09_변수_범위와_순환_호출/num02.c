893f7bccf91bf8f672873170fd71e2bddc4b9191
among5094 <106166621+among5094@users.noreply.github.com>
2024-09-04 02:40:14 +0900

    Add files via upload

--- CH09_변수_범위와_순환_호출/num02.c
+++ CH09_변수_범위와_순환_호출/num02.c
@@ -1,74 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-// 9장 연습문제 2번
 
-// 함수 선언
-int get_dice_face(void);
-void print_roll_counts(void);
+// 주사위를 던져서 1부터 6까지의 숫자를 반환하는 함수
+int get_dice_face() {
+    return (rand() % 6) + 1; // 1부터 6까지의 난수 생성
+}
 
-// 각 주사위 눈의 횟수를 저장할 전역 변수
-int count_1 = 0;
-int count_2 = 0;
-int count_3 = 0;
-int count_4 = 0;
-int count_5 = 0;
-int count_6 = 0;
-
-// 메인 함수
 int main() {
-    int total_rolls = 0;
+    // 정적 지역 변수임 
+    static int count[6] = {0}; // 각 주사위 면의 횟수를 저장할 배열 (0~5번 인덱스는 각각 1~6의 면을 의미)
+    int i; //반복문에서 사용되는 루프 카운터
+    int face; //주사위를 던졌을 때 나온 주사위의 면
 
-    // 랜덤 시드 설정
+    // 난수 생성 초기화
     srand(time(0));
 
-    // 주사위를 100번 던지기 위한 루프
-    while (total_rolls < 100) {
-        get_dice_face(); //주사위 눈을 결정하고, 해당 눈의 횟수를 증가시키는 함수
-        total_rolls++; //주사위를 던진 횟수를 증가
+    // 주사위를 100번 던지기
+    for (i = 0; i < 100; i++) {
+        face = get_dice_face(); // 주사위 던지기(함수를 호출하여 주사위를 던진 결과를 face 변수에 저장)
+        count[face - 1]++; // 나온 면의 횟수를 증가(face가 나타난 횟수를 기록)
     }
 
-    // 주사위 눈의 횟수 출력
-    print_roll_counts();
+    // 결과 출력
+    printf("주사위 결과:\n");
+    for (i = 0; i < 6; i++) {
+        printf("%d=%d\n", i + 1, count[i]); // 각 면의 횟수를 출력
+    }
 
     return 0;
 }
+/*
+ i + 1을 사용하는 이유는 배열의 인덱스와 주사위 면의 숫자 간의 차이를 맞추기 위해서
+ 배열 인덱스는 0부터 시작하는데 주사위 면은 1부터 6까지니까
 
-// 주사위 눈을 반환하는 함수 구현
-int get_dice_face(void) {
-
-    int face = rand() % 6 + 1;
-
-    // 각 눈의 횟수를 증가
-    switch (face) {
-        case 1:
-            count_1++;
-            break;
-        case 2:
-            count_2++;
-            break;
-        case 3:
-            count_3++;
-            break;
-        case 4:
-            count_4++;
-            break;
-        case 5:
-            count_5++;
-            break;
-        case 6:
-            count_6++;
-            break;
-    }
-    return face;
-}
-
-// 주사위 눈의 횟수를 출력하는 함수 구현
-void print_roll_counts(void) {
-    printf("1의 눈: %d번\n", count_1);
-    printf("2의 눈: %d번\n", count_2);
-    printf("3의 눈: %d번\n", count_3);
-    printf("4의 눈: %d번\n", count_4);
-    printf("5의 눈: %d번\n", count_5);
-    printf("6의 눈: %d번\n", count_6);
-}
+*/

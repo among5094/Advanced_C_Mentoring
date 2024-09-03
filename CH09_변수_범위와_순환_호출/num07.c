a0f084423b22f4ad48caa643b90cf4e9a70437e8
among5094 <106166621+among5094@users.noreply.github.com>
2024-09-04 02:47:12 +0900

    Add files via upload

--- CH09_변수_범위와_순환_호출/num07.c
+++ CH09_변수_범위와_순환_호출/num07.c
@@ -5,7 +5,8 @@ int power(int base, int power_raised);
 
 // 메인 함수
 int main() {
-    int base, power_raised;
+    int base; // 밑수
+    int power_raised; // 지수
 
     printf("밑수: ");
     scanf("%d", &base);
@@ -20,9 +21,10 @@ int main() {
 
 // power 함수 구현 (재귀 함수)
 int power(int base, int power_raised) {
-    if (power_raised == 0) {
-        return 1; // 기저 조건: 지수가 0이면 1을 반환
+    if (power_raised == 0) { // 조건: 재귀 호출을 종료하는 조건(지수가 0일 때 결과는 1)
+        return 1; // 지수가 0이면 1을 반환
     } else {
-        return base * power(base, power_raised - 1); // 재귀 호출
+        return base * power(base, power_raised - 1);
+        // 재귀 호출(power_raised - 1은 지수를 1씩 줄여서 재귀 호출을 진행)
     }
 }

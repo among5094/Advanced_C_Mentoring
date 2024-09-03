65ca70fbf4ae7de442a44d05f4f444b0461d80f6
among5094 <106166621+among5094@users.noreply.github.com>
2024-09-04 02:58:00 +0900

    Add files via upload

--- CH09_변수_범위와_순환_호출/num10.c
+++ CH09_변수_범위와_순환_호출/num10.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// 10번
+// 10번(조화수열의 합 문제)
 
 double harmonic_sum(int n);
 
@@ -7,11 +7,14 @@ int main() {
     int n;
     double result;
 
+    // 사용자로부터 정수를 입력받음
     printf("정수를 입력하십시오: ");
     scanf("%d", &n);
 
-    // 수열의 값을 계산하여 출력
+    // 입력받은 정수를 사용하여 수열의 값을 계산
     result = harmonic_sum(n);
+
+    // 계산된 결과를 출력
     printf("%lf\n",result);
 
     return 0;
@@ -19,6 +22,8 @@ int main() {
 
 // harmonic_sum 함수 (재귀 함수)
 double harmonic_sum(int n) {
+
+    // 기저 조건: n이 1일 때, 결과는 1.0(n이 1일 때, 조화수열의 합을 직접 계산할 수 있으므로)
     if (n == 1) {
         return 1.0; // 조건: n이 1이면 1.0을 반환
     } else {
